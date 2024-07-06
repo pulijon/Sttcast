d7ac741cc8ede3c7592906ba06a1a2308c70a603
José Miguel Robles <jmrobles@alaveradeviriato.net>
2024-07-06 17:40:49 +0200

    Change batch script for multifile

--- batch_stt.bash
+++ batch_stt.bash
@@ -12,52 +12,92 @@
 
 srcdir=$1
 prcdir=/mnt/ram
+vosk_suffix="vosk"
+whisper_suffix="whisper"
+audio_suffix="audio"
 
-export IFS=$'\n'
+mp3_vosk_files=()
+mp3_whisper_files=()
+html_vosk_files=()
+html_whisper_files=()
+audio_vosk_files=()
+whisper_vosk_files=()
+meta_vosk_files=()
+meta_whisper_files=()
 
-for episode in $(find "$srcdir" -maxdepth 1 -name "*.mp3" | sort -r)
+for episode in $(find "$srcdir" -maxdepth 1 -type f -name "*.mp3" | sort -r)
 do
 	mp3=$(basename $episode)
 	ep=${mp3%.mp3}
-	html=$ep.html
-	html_audio=${ep}_audio.html
-	html_whisper=${ep}_whisper.html
-	html_whisper_audio=${ep}_whisper_audio.html
-	if [ ! -f "$srcdir/$mp3" ]
-	then
-		echo No existe "$srcdir/$mp3"
-		continue
-	fi
+	meta=${ep}.meta
+	html_vosk=${ep}_${vosk_suffix}.html
+	html_vosk_audio=${ep}_${vosk_suffix}_${audio_suffix}.html
+	html_whisper=${ep}_${whisper_suffix}.html
+	html_whisper_audio=${ep}_${whisper_suffix}_${audio_suffix}.html
 
-	cp "$srcdir/$mp3" "$prcdir"
-
-	if [ ! -f "$srcdir/$html" ]
+	if [ ! -f "${srcdir}/${html_vosk}" ]
 	then
-		echo Processing with vosk "$srcdir/$mp3"
-		cpus=$(cat cpus.txt | tr -d '\n')
-		seconds=$(cat seconds.txt | tr -d '\n')
-		echo Trabajando con $cpus CPUs y $seconds segundos
-		./sttcast.py --seconds $seconds --hconf 0.95 --mconf 0.70 --lconf 0.50 --overlap 2 --cpus $cpus --min-offset 60 --max-gap 0.8 $prcdir/$mp3
-		cp "$prcdir/$html" "$srcdir"
-	 	./add_audio_tag.py --mp3-file "$mp3" -o "$prcdir/$html_audio" "$prcdir/$html"
-        cp "$prcdir/$html_audio" "$srcdir"
-		meta=$ep.meta
-		cp "$prcdir/$meta"  "$srcdir"
+		if [ ! -f "${prcdir}/${mp3}" ]
+		then
+			cp "${srcdir}/${mp3}" "${prcdir}"
+		fi
+		mp3_vosk_files+=("${prcdir}/${mp3}")
+		html_vosk_files+=("${prcdir}/${html_vosk}")
+		audio_vosk_files+=("${prcdir}/${html_vosk_audio}")
+    	meta_vosk_files+=("${prcdir}/${meta}")
 	fi
-
-	if [ ! -f "$srcdir/$html_whisper" ]
+	
+	if [ ! -f "${srcdir}/${html_whisper}" ]
 	then
-        echo Processing with whisper "$srcdir/$mp3"
-		./sttcast.py --seconds 4000 --whisper --whmodel small --cpus 1 --html-file $prcdir/$html_whisper --min-offset 60 --max-gap 0.8   $prcdir/$mp3
-		cp "$prcdir/$html_whisper" "$srcdir"
-	 	./add_audio_tag.py --mp3-file "$mp3" -o "$prcdir/$html_whisper_audio" "$prcdir/$html_whisper"
-        cp "$prcdir/$html_whisper_audio" "$srcdir"
-		meta=$ep.meta
-		cp "$prcdir/$meta"  "$srcdir"
-	fi
-
-	if [ -f $prcdir/$mp3 ]
-	then
-		rm $prcdir/${ep}*
-	fi
+		if [ ! -f "${prcdir}/${mp3}" ]
+		then
+			cp "${srcdir}/${mp3}" "${prcdir}"
+		fi
+		mp3_whisper_files+=("${prcdir}/${mp3}")
+		html_whisper_files+=("${prcdir}/${html_whisper}")
+		audio_whisper_files+=("${prcdir}/${html_whisper_audio}")
+    	meta_whisper_files+=("${prcdir}/${meta}")
+    fi
 done
+
+if [ ${#mp3_vosk_files[*]} -gt 0 ]
+then
+	cpus=$(cat cpus.txt | tr -d '\n')
+	seconds=$(cat seconds.txt | tr -d '\n')
+	Procesando con vosk ${mp3_vosk_files[*]}
+	echo Trabajando con $cpus CPUs y $seconds segundos
+	echo "${mp3_vosk_files[*]}"
+	./sttcast.py --seconds $seconds --cpus $cpus  --html-suffix ${vosk_suffix} ${mp3_vosk_files[*]}
+	echo Fin de la transcripción con vosk
+	echo Creación de etiquetas de audio para los ficheros vosk y obtención de resultados
+	for i in "${!mp3_vosk_files[@]}"
+	do
+		cp "${html_vosk_files[$i]}" "${srcdir}"
+		cp "${meta_vosk_files[$i]}" "${srcdir}"
+		echo ./add_audio_tag.py --mp3-file "${mp3_vosk_files[$i]}" -o "${audio_vosk_files[$i]}" "${html_vosk_files[$i]}"
+
+		./add_audio_tag.py --mp3-file "${mp3_vosk_files[$i]}" -o "${audio_vosk_files[$i]}" "${html_vosk_files[$i]}"
+		cp "${audio_vosk_files[$i]}" "${srcdir}"
+    	rm "${html_vosk_files[$i]}"
+		rm "${audio_vosk_files[$i]}"
+	done
+fi
+
+if [ ${#mp3_whisper_files[*]} -gt 0 ]
+then
+	echo Procesando con whisper ${mp3_whisper_files[*]}
+	./sttcast.py --seconds 15000 --whisper --whmodel small --cpus 1 --html-suffix ${whisper_suffix} ${mp3_whisper_files[*]}
+	echo Fin de la transcripción con vosk
+	echo Creación de etiquetas de audio para los ficheros whisper y obtención de resultados
+	for i in "${!mp3_whisper_files[@]}"
+	do
+		cp "${html_whisper_files[$i]}" "${srcdir}"
+		rm "${meta_whisper_files[$i]}"
+		cp "${meta_whisper_files[$i]}" "${srcdir}"
+		echo ./add_audio_tag.py --mp3-file "${mp3_whisper_files[$i]}" -o "${audio_whisper_files[$i]}" "${html_whisper_files[i]}"
+		./add_audio_tag.py --mp3-file "${mp3_whisper_files[$i]}" -o "${audio_whisper_files[$i]}" "${html_whisper_files[$i]}"
+		cp "${audio_whisper_files[$i]}" "${srcdir}"
+    	rm "${html_whisper_files[$i]}"
+		rm "${audio_whisper_files[$i]}"
+	done
+fi

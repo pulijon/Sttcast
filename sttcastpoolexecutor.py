2f51b18efa733e69580702784d53405f4134803c
José Miguel Robles <jmrobles@alaveradeviriato.net>
2024-07-12 19:34:59 +0200

    Pool map function does not like objects
    Instance methods are converted in functions that
    get data from the instance as parameters

--- sttcastpoolexecutor.py
+++ sttcastpoolexecutor.py
@@ -1,23 +1,25 @@
 from concurrent.futures import ProcessPoolExecutor
 import logging
+from sttcastvosktasks import SttcastVoskTaskSet
+from sttcastwhispertasks import SttcastWhisperTaskSet
+
+def task_work(task):
+    return task["func"](task["data"])
 
 class SttcastPoolExecutor(ProcessPoolExecutor):
-    def __init__(self, cpus):
+    def __init__(self, cpus, par_list):
         super().__init__(max_workers=cpus)
-        self.cpus = cpus
         self.tasks = []
+        for par in par_list:
+            task_set_class = SttcastWhisperTaskSet if par["whisper"] else SttcastVoskTaskSet
+            tset = task_set_class(par)
+            self.tasks.extend(tset.get_tasks())
 
-    def add_task(self, task):
-        self.tasks.append(task)
-
-    def task_work(self, task):
-        return task.task_work()
+    # def task_work(self, task):
+    #     return task["func"].task_work()
     
-    def get_tasks(self):
-        return self.tasks
-
     def execute_tasks(self):
         with self as executor:
-            for f, t in executor.map(self.task_work, self.tasks):
+            for f, t in executor.map(task_work, self.tasks):
                 logging.info(f"{f} ha tardado {t}")
 
